314cc5ce3f65de75fce99f455c2ceab4d2b73a00
JoaolimaDev <ozymandiasphp@gmail.com>
2024-10-28 16:58:46 -0300

    criação do componenete courses, e roteamento

--- crud-angular/src/app/app.component.ts
+++ crud-angular/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule,
+    MatIconModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
