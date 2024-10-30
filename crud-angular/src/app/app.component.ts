94a9dab550a799b77c655bded149dabeacae1898
JoaolimaDev <ozymandiasphp@gmail.com>
2024-10-30 20:05:14 -0300

    Containerização do app MONGODB atualizações na camada de serviço

--- crud-angular/src/app/app.component.ts
+++ crud-angular/src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, MatToolbarModule,
-    MatIconModule, HttpClientModule
+    MatIconModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -8,6 +8,7 @@ import { Injectable } from '@angular/core';
 export class CoursesService {
 
 
+  constructor(private http: HttpClient) {}
 
 
   list(): Course[] {
